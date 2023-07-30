7b320e15b32651bec549ac4501c4944f1f4263be
Gyeong jun Son <thsrudwns1214@gmail.com>
2023-07-30 14:53:23 +0000

    vanilla js 연습 시작

---
+++ main.js
@@ -0,0 +1,19 @@
+const calculater = {
+	plus: function(a,b){
+		console.log(a+b);
+	},
+	subtract: function(a,b){
+		console.log(a-b);
+	},
+	multiply: function(a,b){
+		console.log(a*b);
+	},
+	divide: function(a,b){
+		console.log(a/b);
+	}
+};
+
+calculater.plus(5,4);
+calculater.subtract(5,4);
+calculater.multiply(5,4);
+calculater.divide(5,4);
