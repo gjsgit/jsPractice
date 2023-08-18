b59081b016523d8c6111c5993cad21591beae782
Gyeong jun Son <thsrudwns1214@gmail.com>
2023-08-18 14:39:23 +0000

    japanese End

--- randomJapanese/randomTest.js
+++ randomJapanese/randomTest.js
@@ -81,3 +81,18 @@ function giveVoca(){
 vocaButton.addEventListener("click", giveVoca);
 plusButton.addEventListener("click", plus);
 subtractButton.addEventListener("click", subtract);
+
+//타이머
+const timer = document.querySelector(".timer");
+
+function getTimer(){
+	const date = new Date();
+	const hours = String(date.getHours()).padStart(2,"0");
+	const minutes = String(date.getMinutes()).padStart(2,"0");
+	const seconds = String(date.getSeconds()).padStart(2,"0");
+	
+	timer.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getTimer()
+setInterval(getTimer,1000);
